5f63dac1eefb55e00a8ac9ee9453d9785ba0480f
iagosm <iagosousa201486@gmail.com>
2024-10-29 21:56:30 -0300

    atualizacao nas paginas e na auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                         @csrf
 
                         <div class="form-group mb-3">
-                            <label for="email" class="form-label">{{ __('Email Address') }}</label>
+                            <label for="email" class="form-label">{{ __('E-mail') }}</label>
                             <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -34,7 +34,7 @@
                         </div>
 
                         <div class="form-group mb-3">
-                            <label for="password" class="form-label">{{ __('Password') }}</label>
+                            <label for="password" class="form-label">{{ __('Senha') }}</label>
                             <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -46,26 +46,26 @@
                         <div class="form-check mb-3">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                             <label class="form-check-label" for="remember">
-                                {{ __('Remember Me') }}
+                                {{ __('Lembrar de mim') }}
                             </label>
                         </div>
 
                         <div class="d-grid">
                             <button type="submit" class="btn btn-success">
-                                {{ __('Login') }}
+                                {{ __('Entrar') }}
                             </button>
                         </div>
 
                         @if (Route::has('password.request'))
                             <div class="text-center mt-3">
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
+                                    {{ __('Esqueceu a senha?') }}
                                 </a>
                             </div>
                         @endif
 
                         <div class="text-center mt-3">
-                            <a href="{{ route('register') }}">{{ __('Create an Account') }}</a>
+                            <a href="{{ route('register') }}">{{ __('Criar Conta') }}</a>
                         </div>
                     </form>
                 </div>
